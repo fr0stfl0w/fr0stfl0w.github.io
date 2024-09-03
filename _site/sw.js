12c0d63661dd50eda25b14b00e3a8320029f2ce3
erikavasnormandy <erikavasnormandy@gmail.com>
2024-09-03 11:12:14 -0700

    fixed images

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240902.191837';
+const cacheName = 'chirpy-20240903.181119';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

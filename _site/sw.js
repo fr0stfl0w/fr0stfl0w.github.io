d7fe8a5fdaa4cdbfa06ce7d6d9f15b4aaa697ea0
erikavasnormandy <erikavasnormandy@gmail.com>
2025-04-28 09:48:02 -0700

    added more content to the AI and ML page

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250428.153505';
+const cacheName = 'chirpy-20250428.164624';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
